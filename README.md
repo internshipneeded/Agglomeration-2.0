ac089e7a3a2279cd3a6e44081bc60f61f4759d8d
ADI-coder-TYA <thisisrealadi@gmail.com>
2026-01-24 15:43:25 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pet_perplexity
+
+This flutter project is meant to satisfy the requirements for Agglomeration 2.0 Pet Perplexity Problem Statement.
---
+++ android/app/src/main/kotlin/com/internshipneeded/pet_perplexity/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.internshipneeded.pet_perplexity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
